13039e0d00560d563fec4cbf8e4dbad3ad37b3b5
ecarpenterfromalabama <u964010154@byui.edu>
2022-09-20 10:51:46 -0500

    Updated to dynamically change copyright year and removed bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 let lastMod = new Date(document.lastModified);
+let currentCopy = new Date().getFullYear();
 
 let modifiedDate = document.getElementById("last-modified");
 modifiedDate.innerHTML = lastMod.toLocaleString();
+
+let copyDate = document.getElementById("copy-year");
+copyDate.innerHTML = currentCopy;
