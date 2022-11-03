1b1960aeff91186549cbb2f57978e3a4a3ba6f4e
ecarpenterfromalabama <u964010154@byui.edu>
2022-11-03 07:56:30 -0500

    working on join page

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -8,4 +8,17 @@ var day = todayDate.toLocaleString("default", { day: "2-digit" });
 // Generate yyyy-mm-dd date string
 var formattedDate = year + "-" + month + "-" +day ;
 const formDate = document.getElementById("date");
-formDate.setAttribute("value", formattedDate);
+formDate.setAttribute("value", formattedDate); //Breaks on thankyou.html
+
+// Hold Submittee 
+function holdName(){
+    const formName = document.querySelector('[name="fname"]').value;
+    localStorage.setItem("submitName", formName);
+}
+
+const subBtn = document.querySelector(".submitBtn");
+
+subBtn.addEventListener("click", holdName); //Breaks on thankyou.html
+console.log("test");
+let currentName = window.localStorage.getItem("submitName");
+document.getElementById("submitee-name").textContent = currentName;
